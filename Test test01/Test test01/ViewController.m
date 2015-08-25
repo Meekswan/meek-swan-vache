9ec1ebc248c8edb2b49579e96a86f8dedf0d06ff
Yore <847717426@qq.com>
2015-08-25 16:39:03 +0800

    this project is just for testing

---
+++ Test test01/Test test01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test test01
+//
+//  Created by lan on 15/8/25.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"this project is just for testing");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
